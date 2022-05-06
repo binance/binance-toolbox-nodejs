778f3614c678c103539cdcc3d58944ab974a1e4b
Liang Shi <liang.shi@protonmail.com>
2022-05-06 15:04:31 +1000

    update connector version
    latest update

--- src/place-order.js
+++ src/place-order.js
@@ -2,7 +2,7 @@
 /*
 * This is example script that showing how to use some of the basic methods from @binance/connector
 * the repo of the library: https://github.com/binance/binance-connector-node, which has a folder with example files for each method
-* 
+*
 * It's possible to see error from any of these methods, reasons could be:
 * - Not enough balance in the account
 * - Wrong parameters
@@ -10,13 +10,17 @@
 *
 */
 
-
 const { Spot } = require('@binance/connector')
+
+// You could change the baseURL to switch between testnet and mainnet, don't forget to change the APIKEY and APISECRET
+// const baseURL = "https://api.binance.com"  for mainnet
+// const baseURL = "https://testnet.binance.vision" for testnet
+const baseURL = "https://testnet.binance.vision"
 
 const apiKey = process.env.APIKEY || ''
 const apiSecret = process.env.APISECRET || ''
 
-const client = new Spot(apiKey, apiSecret)
+const client = new Spot(apiKey, apiSecret, { baseURL })
 
 async function app() {
 
@@ -25,14 +29,14 @@ async function app() {
 
   const orderId =  await client.newOrder('BNBUSDT', 'BUY', 'LIMIT', {
     price: 400,
-    quantity: 0.1,
+    quantity: "0.2",
     timeInForce: 'GTC'
   }).then(response => response.data.orderId)
   .catch(err => console.error(err.response.data))
-  
+
   console.log(`The new order id: ${orderId}`)
   console.log("finished placing order")
-  
+
   // get order details
   console.log("start to query order details")
   const order_details = await client.getOrder('BNBUSDT', {
@@ -44,6 +48,7 @@ async function app() {
   console.log("start to cancel the order if it's still in open status")
   await client.cancelOrder('BNBUSDT', { orderId })
   .then(response => console.log(response.data))
+  .catch(err => console.error(err.response.data))
 }
 
 app()
